75faf5f9036ed7eaeb9597d2b2ff0642614a51bd
Norman Feske <norman.feske@genode-labs.com>
2015-03-20 18:46:32 +0100

    Spec: generate class descriptions

--- manual/spec/util.tcl
+++ manual/spec/util.tcl
@@ -74,7 +74,7 @@ proc import_tokens {file_name} {
 ##
 # Return type of specified token
 #
-proc tok_type {token} {
+proc tok_type { token } {
 	regexp {[a-z]+} $token type
 	return $type
 }
@@ -83,9 +83,14 @@ proc tok_type {token} {
 ##
 # Return text of specified token
 #
-proc tok_text {name} {
+proc tok_text { token } {
 	global token_text
-	return $token_text($name)
+	return $token_text($token)
+}
+
+
+proc tok_valid { token } {
+	return [string length $token]
 }
 
 
@@ -98,7 +103,7 @@ proc tok_text {name} {
 # \param plain_var      variable name of the 'plain_fn' argument
 # \param sub_token_var  variable name of the 'sub_token_fn' argument
 #
-proc foreach_sub_token {sequence plain_var plain_fn sub_token_var sub_token_fn} {
+proc foreach_sub_token { sequence plain_var plain_fn sub_token_var sub_token_fn } {
 
 	upvar $plain_var     plain
 	upvar $sub_token_var sub_token
@@ -127,6 +132,8 @@ proc unfold_token {{token content0}} {
 
 	set output ""
 
+	if {![tok_valid $token]} { return "" }
+
 	foreach_sub_token [tok_text $token] plain {
 
 		# perform character substitutions for LaTeX compliance
@@ -146,6 +153,7 @@ proc unfold_token {{token content0}} {
 #
 proc sub_token {token token_type} {
 
+	if {$token == ""} { return "" }
 	if {[regexp "§($token_type\\d+)°" [tok_text $token] dummy sub_token]} {
 		return $sub_token
 	} else {
@@ -243,4 +251,542 @@ proc public_base_classes { class_token } {
 	return $base_classes
 }
 
+
+##
+# Return true if multi-line comment starts with "/**"
+#
+proc mlcomment_is_public { mlcomment } {
+
+	return [regexp {^\s*/\*\*\n} $mlcomment]
+}
+
+
+##
+# Parse multi-line comment
+#
+# The function returns a list of comment parts. A part may be a paragraph or a
+# item that starts with "/".
+#
+proc parse_mlcomment { mlcomment } {
+
+	# eliminate leading and trailing comment characters (/** and */)
+	regsub {^/\*+\n} $mlcomment {} mlcomment
+	regsub {\s*\*/$} $mlcomment {} mlcomment
+
+	# list of parts
+	set result {}
+
+	# content of current part
+	set part ""
+
+	foreach line [split $mlcomment "\n"] {
+
+		#
+		# Remove leading whitespace and the '*' character. The '*' is normally
+		# followed by a space, except when it is in front of an empty line.
+		#
+		regsub {^\s*\*} $line "" line
+		regsub {^ }    $line "" line
+		regsub {^\s+$} $line "" line
+
+		# detect beginning of a new part
+		if {$line == "" || [regexp {^\\} $line dummy]} {
+
+			# append non-empty part to list
+			if {$part != ""} {
+				lappend result $part }
+
+			# begin new part
+			set part ""
+		}
+
+		#
+		# Append current line to current part. Remove leading whitespace
+		# that may be present if parameter descriptions span multiple lines.
+		#
+		regsub {^\s+} $line " " line
+
+		if {$line != ""} {
+			set part [concat $part " " $line] }
+	}
+
+	lappend result $part
+
+	return $result
+}
+
+
+##
+# Return the number of top-level classes in the header file
+#
+proc number_of_top_level_classes { } {
+
+	set count 0
+	foreach token_type { class struct classtpl structtpl } {
+		incr count [regsub -all "§$token_type\\d+°" [tok_text content0] {} dummy]
+	}
+	return $count
+}
+
+
+##
+# Return token for the multi-line comment in the file header
+#
+proc header_comment { } {
+
+	set header_token [sub_token content0 mlcomment]
+
+	if {[tok_valid $header_token]} {
+		return [parse_mlcomment [tok_text $header_token]] }
+
+	return ""
+}
+
+
+##
+# Return parsed multi-line comment for a function
+#
+proc function_comment { func_token } {
+
+	set mlcomment_token [sub_token $func_token mlcomment]
+	if {[tok_valid $mlcomment_token]} {
+		return [parse_mlcomment [tok_text $mlcomment_token]] }
+
+	return ""
+}
+
+
+##
+# Return the brief description of a class
+#
+proc brief_class_description { class_token } {
+
+	#
+	# If the class is prepended with a multi-line comment, take the
+	# brief description from there
+	#
+	set comment_token [sub_token $class_token mlcomment]
+	if {[tok_valid $comment_token]} {
+
+		set mlcomment [tok_text $comment_token]
+		if {[mlcomment_is_public $mlcomment]} {
+
+			set mlcomment_parts [parse_mlcomment $mlcomment]
+			if {[llength $mlcomment_parts]} {
+				return [lindex $mlcomment_parts 0]
+			}
+		}
+	}
+
+	#
+	# Class lacks a valid multi-line comment. If the class is the only
+	# class contained in the header, we use the header's brief description
+	#
+	if {[number_of_top_level_classes] == 1} {
+
+		# search the mlcomment of the file header for a brief description
+		foreach part [header_comment] {
+
+			if {[regexp {^\\brief\s} $part]} {
+				regsub {^\\brief\s+} $part "" part
+				return $part
+			}
+		}
+	}
+	return ""
+}
+
+
+##
+# Return true if class contains RPC annotations
+#
+proc class_is_rpc_interface { class_token } {
+
+	set classblock_token [sub_token $class_token classblock]
+	if {$classblock_token == ""} { return 0 }
+	if {[sub_token $classblock_token genoderpc] != ""} { return 1 }
+	return 0
+}
+
+
+##
+# Return function name
+#
+proc function_name { func_token } {
+
+	if {[is_function $func_token]} {
+		set funcsignature_token [sub_token $func_token          funcsignature]
+		set name_token          [sub_token $funcsignature_token identifier]
+		set tilde ""
+		if {[sub_token $func_token tilde] != ""} { set tilde "~" }
+		return "$tilde[unfold_token $name_token]"
+	}
+}
+
+
+##
+# Return the return type of a function
+#
+proc function_return_type { func_token } {
+
+	if {[is_function $func_token]} {
+		set retval_token [sub_token $func_token retval]
+		if {$retval_token != ""} {
+			return [concat [unfold_token $retval_token]] }
+	}
+
+	return ""
+}
+
+
+##
+# Return the multi-line comment of a compound token
+#
+proc mlcomment_parts { token } {
+
+	set comment_token [sub_token $token mlcomment]
+	if {[tok_valid $comment_token]} {
+
+		set mlcomment [tok_text $comment_token]
+		if {[mlcomment_is_public $mlcomment]} {
+
+			return [parse_mlcomment $mlcomment]
+		}
+	}
+
+	return {}
+}
+
+
+##
+# Return the brief description of a function
+#
+proc function_brief_description { function_token } {
+
+	set mlcomment_parts [mlcomment_parts $function_token]
+	if {[llength $mlcomment_parts]} {
+
+		set brief [concat [lindex $mlcomment_parts 0]]
+
+		if {$brief == "Constructor" || $brief == "Destructor"} {
+			return "" }
+
+		#
+		# Filter out brief descriptions starting with "Return" or "Returns".
+		# This information is captured by 'function_return_value' instead.
+		#
+		if {[regexp {^Returns?\s+} $brief]} { return "" }
+
+		return $brief
+	}
+	return ""
+}
+
+
+##
+# Return list of paragraphs of the detailed description of a function
+#
+proc function_detailed_description { function_token } {
+
+	set result {}
+	set first 1
+	foreach part [mlcomment_parts $function_token] {
+
+		if {!$first && ![regexp {^\\} $part]} {
+			lappend result $part }
+
+		set first 0
+	}
+	return $result
+}
+
+
+##
+# Return true if token is a function
+#
+proc is_function { token } {
+
+	foreach token_type { funcdecl funcimpl constdecl constimpl destdecl destimpl } {
+		if {[tok_type $token] == "$token_type"} { return 1 } }
+
+	return 0
+}
+
+
+##
+# Return true if function is specified modifier
+#
+proc function_has_modifier { func_token modifier } {
+
+	set modifier_token [sub_token $func_token modifier]
+	if {$modifier_token != ""} {
+		set modifiers [unfold_token $modifier_token]
+		return [regexp $modifier $modifiers]
+	}
+
+	return 0
+}
+
+
+proc function_is_static { func_token } {
+	return [function_has_modifier $func_token static] }
+
+
+proc function_is_virtual { func_token } {
+	return [function_has_modifier $func_token virtual] }
+
+
+proc function_is_pure_virtual { func_token } {
+	return [expr {[sub_token $func_token virtassign] != ""}] }
+
+
+proc function_is_const { func_token } {
+	return [expr {[sub_token $func_token keyconst] != ""}] }
+
+
+##
+# Return true if the method is an accessor (getter)
+#
+# An accessor is an argument-less const function with a return value.
+#
+proc function_is_accessor { func_token } {
+
+	if {[function_is_static $func_token]}                { return 0; }
+	if {![function_is_const $func_token]}                { return 0; }
+	if {[llength [function_arguments $func_token]] != 0} { return 0; }
+	if {[function_return_type $func_token] == "void"}    { return 0; }
+	if {[function_return_type $func_token] == ""}        { return 0; }
+	if {[llength [mlcomment_parts $func_token]] != 0}    { return 0; }
+
+	return 1
+}
+
+
+##
+# Return true if function should be described in detail
+#
+proc function_is_interesting { func_token } {
+
+	#
+	# Skip overrides with no prepending comment. Those are mere implementation
+	# of an abstract interface defined somewhere else.
+	#
+	if {[sub_token $func_token keyoverride] != "" &&
+		[sub_token $func_token mlcomment] == ""} {
+		return 0
+	}
+
+	#
+	# Accessors are listed only at the class description
+	#
+	if {[function_is_accessor $func_token]} { return 0; }
+
+	#
+	# A destructor or constructor without comments is not interesting
+	#
+	foreach func_type { constdecl constimpl destdecl destimpl } {
+		if {[tok_type $func_token] == "$func_type"} {
+			set num_mlcomment_parts [llength [mlcomment_parts $func_token]]
+			if {$num_mlcomment_parts == 0} { return 0 }
+
+			# trivial comments do not make a constructor or destructor interesting
+			if {$num_mlcomment_parts == 1 &&
+			    [function_brief_description $func_token] == ""} { return 0 }
+		}
+	}
+
+	return 1
+}
+
+
+##
+# Return description of function argument as present in the mlcomment
+#
+proc function_argument_description { function_token arg_name } {
+
+	foreach part [function_comment $function_token] {
+		if {[regexp {^\\param\s+([^ ]+)\s+(.*)$} $part dummy name desc]} {
+			if {$name == $arg_name} {
+				return [string totitle $desc]
+			}
+		}
+	}
+	return ""
+}
+
+
+##
+# Return list of function arguments
+#
+# Each list item is a list of name, type, default, and description.
+#
+proc function_arguments { func_token } {
+
+	set funcsignature_token [sub_token $func_token          funcsignature]
+	set argparenblk_token   [sub_token $funcsignature_token argparenblk]
+
+	if {$argparenblk_token == ""} { return {} }
+
+	set args {}
+	foreach_sub_token [tok_text $argparenblk_token] plain { } token {
+
+		if {[tok_type $token] == "argdecl"} {
+
+			set arg_type [unfold_token [sub_token $token argtype]]
+
+			#
+			# Determine argument name
+			#
+			set arg_name ""
+			if {[tok_type $token] == "argdecl"} {
+				set arg_name [unfold_token [sub_token $token argname]]
+
+				# try to infer the argument name from commented-out name
+				if {$arg_name == ""} {
+					set arg_lcomment [unfold_token [sub_token $token lcomment]]
+					regexp {/\*\s*(.+)\s*\*/} $arg_lcomment dummy arg_name
+				}
+			}
+			if {$arg_name == ""} { set arg_name "-" }
+
+			#
+			# Determine default value
+			#
+			set arg_default ""
+			set arg_default [unfold_token [sub_token $token argdefault]]
+			regsub {=\s*} $arg_default "" arg_default
+
+			#
+			# Determine argument description from the function's mlcomment
+			#
+			set arg_desc [function_argument_description $func_token $arg_name]
+
+			lappend args [list $arg_name $arg_type $arg_default $arg_desc]
+		}
+	}
+	return $args
+}
+
+
+##
+# Return information about the function's return value
+#
+proc function_return_description { func_token } {
+
+	set retval_token [sub_token $func_token retval]
+
+	set ret_desc ""
+
+	# look for \return annotation in the multi-line comment
+	foreach part [function_comment $func_token] {
+		if {[regexp {^\\return\s+(.*)$} $part dummy desc]} {
+			set ret_desc [string totitle $desc] } }
+
+	#
+	# If not \return annotation was found, look if the brief description
+	# starts with "Return" or "Returns"
+	#
+	if {$ret_desc == ""} {
+		set mlcomment_parts [mlcomment_parts $func_token]
+		if {[llength $mlcomment_parts]} {
+
+			set first_mlcomment_part [concat [lindex $mlcomment_parts 0]]
+
+			if {[regexp {^Returns?\s+(.*)} $first_mlcomment_part dummy brief]} {
+				set ret_desc [string totitle $brief]
+			}
+		}
+	}
+
+	return [concat $ret_desc]
+}
+
+
+##
+# Return list of function-exception infos
+#
+# Each list item is a list of the exception type and description.
+#
+proc function_exceptions { func_token } {
+
+	set exc_list {}
+	foreach part [mlcomment_parts $func_token] {
+
+		if {[regexp {^\\throw\s+([^ ]+)\s*(.*)} $part dummy exc_type exc_desc]} {
+			lappend exc_list [list $exc_type $exc_desc]
+		}
+	}
+	return $exc_list
+}
+
+
+##
+# Extract sequence of public members from class or struct tokens
+#
+proc public_class_members { class_token } {
+
+	if {[tok_type $class_token] == "tplclass"} {
+		set class_token [sub_token $class_token class] }
+
+	if {[tok_type $class_token] == "tplstruct"} {
+		set class_token [sub_token $class_token struct] }
+
+	set classblock_token [sub_token $class_token classblock]
+
+	set members_sequence {}
+	if {[tok_type $class_token] == "struct"} {
+
+		foreach_sub_token [tok_text $classblock_token] plain { } token {
+			if {[tok_type $token] == "declseq"} {
+				append members_sequence [tok_text $token]
+			}
+		}
+	}
+
+	if {[tok_type $class_token] == "class"} {
+
+		foreach_sub_token [tok_text $classblock_token] plain { } prot_token {
+			if {[tok_type $prot_token] == "public"} {
+				foreach_sub_token [tok_text $prot_token] plain { } token {
+					if {[tok_type $token] == "declseq"} {
+						append members_sequence [tok_text $token]
+					}
+				}
+			}
+		}
+	}
+	return $members_sequence
+}
+
+
+##
+# Return list of accessors for given class
+#
+proc accessor_functions { class_token } {
+
+	set accessors {}
+	set members_sequence [public_class_members $class_token]
+	foreach_sub_token $members_sequence plain { } token {
+		if {[is_function $token] && [function_is_accessor $token]} {
+			lappend accessors $token
+		}
+	}
+
+	return $accessors
+}
+
+
+##
+# Return true if class has members
+#
+proc class_has_members { class_token } {
+
+	set members_sequence [public_class_members $class_token]
+	set has_members 0
+	foreach_sub_token $members_sequence plain { } token {
+		if {[is_function $token]} {
+			set has_members 1 } }
+
+	return $has_members
+}
 
