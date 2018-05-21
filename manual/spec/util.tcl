f011d50b4589074874aa2ce937d4c2ddb10456dd
Norman Feske <norman.feske@genode-labs.com>
2018-05-21 15:34:26 +0200

    Update to Genode 18.05
    - Changed boot-loader infrastructure on PC
    - API
      - Unicode handling
      - Expanding_reporter
      - List_model, Buffered_xml
      - One_shot_timeout, Periodic_timeout
    - Init (label_last)
    - Package management

--- manual/spec/util.tcl
+++ manual/spec/util.tcl
@@ -53,7 +53,7 @@ proc get_cmd_arg { arg_name default_value } {
 #
 # XXX evaluate command-line argument
 #
-proc code_url { } { return "https://github.com/genodelabs/genode/blob/17.05/" }
+proc code_url { } { return "https://github.com/genodelabs/genode/blob/18.05/" }
 
 
 ##
